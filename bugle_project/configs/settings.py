f78b0867f74b375e878a9f3bdc2dc6512a6129b4
David Thompson <mail@fatbusinessman.com>
2011-08-23 16:40:51 +0100

    Remove most of the default DB settings.
    They should be in the development settings, when I commit it.

--- bugle_project/configs/settings.py
+++ bugle_project/configs/settings.py
@@ -14,12 +14,12 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'bugle'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'root'             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE = None
+DATABASE_NAME = 'bugle'
+DATABASE_USER = None
+DATABASE_PASSWORD = ''
+DATABASE_HOST = ''
+DATABASE_PORT = ''
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
