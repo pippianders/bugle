ca5c2482cbc62ba1170b8273444bd76d4aa7e887
David Thompson <mail@fatbusinessman.com>
2011-08-24 12:07:52 +0100

    Add live database config overrides.

--- bugle_project/configs/live/settings.py
+++ bugle_project/configs/live/settings.py
@@ -2,3 +2,7 @@ from bugle_project.configs.settings import *
 
 FAYE_URL = 'http://bugle.fort:8001/faye'
 
+DATABASE_ENGINE = 'postgresql_psycopg2'
+DATABASE_NAME = 'bugle'
+DATABASE_USER = 'bugle'
+DATABASE_PASSWORD = 'bugle'
