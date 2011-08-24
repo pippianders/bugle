5a13e905f746e658c9da48522dd0be77ca7494cd
David Thompson <mail@fatbusinessman.com>
2011-08-24 11:40:39 +0100

    Update virtualenv script with new env path.

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [[ -e 'bin/activate' ]]; then
+if [[ -e 'bugle_project_ve' ]]; then
     echo "Looks like you've already got a virtualenv here."
     echo "I'm not going to mess with it."
     exit 1
 fi
 
 echo "Setting up virtualenv"
-virtualenv .
+virtualenv bugle_project_ve
 echo "Installing requirements"
-pip install --use-mirrors --mirrors=http://pypi.fort/ -r requirements.txt
+pip install -E bugle_project_ve --use-mirrors --mirrors=http://pypi.fort/ -r requirements.txt
 
