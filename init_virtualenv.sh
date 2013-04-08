e1c10f9a74a34322c6df841bdb460a67e675c3ef
James Aylett <james@tartarus.org>
2013-04-08 19:52:33 +0100

    ENV as the virtualenv. Also tidy up our use of pip based on latest fort facilities.

--- init_virtualenv.sh
+++ init_virtualenv.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-if [[ -e 'bugle_project_ve' ]]; then
+if [[ -e 'ENV' ]]; then
     echo "Looks like you've already got a virtualenv here."
     echo "I'm not going to mess with it."
     exit 1
 fi
 
 echo "Setting up virtualenv"
-virtualenv bugle_project_ve
+virtualenv ENV
 echo "Installing requirements"
-pip install -E bugle_project_ve --use-mirrors --mirrors=http://pypi.fort/ -r requirements.txt
-
+ENV/bin/pip install -r requirements.txt
